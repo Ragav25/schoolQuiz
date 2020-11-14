7b0dca51826109a6968cc6db57f4503345fdaf97
Ragav25 <ragavravi25@gmail.com>
2020-11-14 17:55:35 +0530

    api-integration-completed

--- quiz/models.py
+++ quiz/models.py
@@ -1,10 +1,13 @@
-from django.db import models
 from django.utils import timezone
+from django.db import models
 
 # Create your models here.
 
 
-class quizes(models.Model):
+class Quizzes(models.Model):
     quiz_name = models.TextField()
     date_created = models.DateTimeField(default=timezone.now)
     category_id = models.TextField()
+
+    def __str__(self):
+        return self.quiz_name
--- quiz/views.py
+++ quiz/views.py
@@ -1,14 +1,56 @@
 import requests
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 
 
 # Create your views here.
 
-def home(request):
-    url = 'https://opentdb.com/api.php?amount=10&category={}&type=multiple'
-    category = 19
+def queryparam_vaild(param):
+    return param != '' and param is not None
 
-    r = requests.get(url.format(category)).json()
-    print(r['results'][0]['question'])
-    return render(request, 'home.html', {'name': 'John'})
+
+def home(request):
+    # CATEGORY URL
+    category_url = requests.get('https://opentdb.com/api_category.php').json()
+
+    # print("&&*****&&", category_url['trivia_categories'])
+
+    categories = []
+
+    for cat in category_url['trivia_categories']:
+        category = {
+            'category_id': cat['id'],
+            'category_name': cat['name']
+        }
+
+        categories.append(category)
+
+    # QUIZ QUESTION URL
+    url = 'https://opentdb.com/api.php?amount=10&category={q_category}&difficulty={q_difficulty}&type=multiple'
+
+    quiz_questions = []
+
+    quiz_category = request.GET.get('quiz_category')
+    quiz_difficulty = request.GET.get('quiz_difficulty')
+
+    if queryparam_vaild(quiz_category) and queryparam_vaild(quiz_difficulty):
+        r = requests.get(url.format(q_category=quiz_category,
+                                    q_difficulty=quiz_difficulty)).json()
+
+        for q in r['results']:
+            questions = {
+                'category': q['category'],
+                'question': q['question'],
+                'correct_answer': q['correct_answer'],
+            }
+
+            quiz_questions.append(questions)
+
+    context = {
+        'quiz_questions': quiz_questions,
+        'categories': categories
+    }
+
+    return render(request, 'home.html', context)
